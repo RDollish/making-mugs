cc63eb9e6acc991ac21b5fb142c4fdb773a62093
Renee Doll <renee@reneedoll.com>
2022-10-07 09:30:06 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ]
+const toFireInKiln = []
+
+for (const piece of clay) {
+   const mug = `${piece} coffee mug`
+   toFireInKiln.push(mug)
+}
+
+console.log(toFireInKiln)
